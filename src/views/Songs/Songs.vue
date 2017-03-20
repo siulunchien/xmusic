d7d13ba019b0ca8467e59eabb2c8dd04a17f8d37
siulunchien <407422007@qq.com>
2017-03-21 00:26:45 +0800

    增加loading

--- src/views/Songs/Songs.vue
+++ src/views/Songs/Songs.vue
@@ -4,42 +4,48 @@
 
 <template>
   <main class="songs">
-    <header class="songs__header">
-      <img :src="topInfo.pic_album" alt="" class="songs__header__pic">
-      <div class="songs__header__title">
-        <h1>{{ topInfo.ListName }}</h1>
-        <p class="f-14">{{ updateTime }} 更新</p>
-      </div>
-      <div class="songs__header__bg"></div>
-    </header>
-    <section class="songs__songs-list">
-      <ul class="songs__songs-list__container">
-        <li v-for="(item, index) in songList" class="songs__songs-list__container__item" @click="chooseSong(item)">
-          <span class="songs__songs-list__container__item__num f-16">{{ index + 1 }}</span>
-          <div class="songs__songs-list__container__item__info">
-            <h1 class="f-16">{{ item.data.songname }}</h1>
-            <p class="f-14">{{ item.data.singer[0].name | formatSinger(item.data.albumname, item.data.albumdesc) }}</p>
+    
+    <div v-if="isLoading" class="songs__loading"><img src="./images/ripple.svg" alt=""></div>
+    <transition name="fade">
+      <div v-if="!isLoading" class="songs__content">
+        <header class="songs__header">
+          <img :src="topInfo.pic_album" alt="" class="songs__header__pic">
+          <div class="songs__header__title">
+            <h1>{{ topInfo.ListName }}</h1>
+            <p class="f-14">{{ updateTime }} 更新</p>
           </div>
-        </li>
-      </ul>
-    </section>
-    <div class="songs__choose">
-      <transition name="fade">
-        <div v-show="isChoose" class="songs__choose__mask" @click="closeChooseSong"></div>
-      </transition>
-      <transition name="slide-fade">
-        <div v-show="isChoose" class="songs__choose__main">
-          <header class="songs__choose__main__header">
-            <h3 v-if="chooseItem" class="f-14"><span>歌曲：</span> {{ chooseItem.data.songname }}</h3>
-          </header>
-          <div class="songs__choose__main__btn-group f-14">
-            <a href="javascript:;" class="songs__choose__main__btn-group__toPlay" @click="immediately">立即播放</a>
-            <a href="javascript:;" class="songs__choose__main__btn-group__addPlayList" @click="addToPlayList">加入播放列表</a>
-            <a href="javascript:;" class="songs__choose__main__btn-group__close" @click="closeChooseSong">关闭</a>
-          </div>
+          <div class="songs__header__bg"></div>
+        </header>
+        <section class="songs__songs-list">
+          <ul class="songs__songs-list__container">
+            <li v-for="(item, index) in songList" class="songs__songs-list__container__item" @click="chooseSong(item)">
+              <span class="songs__songs-list__container__item__num f-16">{{ index + 1 }}</span>
+              <div class="songs__songs-list__container__item__info">
+                <h1 class="f-16">{{ item.data.songname }}</h1>
+                <p class="f-14">{{ item.data.singer[0].name | formatSinger(item.data.albumname, item.data.albumdesc) }}</p>
+              </div>
+            </li>
+          </ul>
+        </section>
+        <div class="songs__choose">
+          <transition name="fade">
+            <div v-show="isChoose" class="songs__choose__mask" @click="closeChooseSong"></div>
+          </transition>
+          <transition name="slide-fade">
+            <div v-show="isChoose" class="songs__choose__main">
+              <header class="songs__choose__main__header">
+                <h3 v-if="chooseItem" class="f-14"><span>歌曲：</span> {{ chooseItem.data.songname }}</h3>
+              </header>
+              <div class="songs__choose__main__btn-group f-14">
+                <a href="javascript:;" class="songs__choose__main__btn-group__toPlay" @click="immediately">立即播放</a>
+                <a href="javascript:;" class="songs__choose__main__btn-group__addPlayList" @click="addToPlayList">加入播放列表</a>
+                <a href="javascript:;" class="songs__choose__main__btn-group__close" @click="closeChooseSong">关闭</a>
+              </div>
+            </div>
+          </transition>
         </div>
-      </transition>
-    </div>
+      </div>
+    </transition>
   </main>
 </template>
 
@@ -57,7 +63,8 @@
         date: '',
         updateTime: '',
         isChoose: false,
-        chooseItem: null
+        chooseItem: null,
+        isLoading: true
       }
     },
     mounted () {
@@ -68,6 +75,7 @@
           this.date = res.date;
           this.updateTime = res.update_time;
           this.songList = res.songlist;
+          this.isLoading = false;
         })
 
     },
